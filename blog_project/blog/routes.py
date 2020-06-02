51910c8743b6d8dd4d26016852f83b8337e7cf09
farhadmpr <farhadmp@gmail.com>
2020-06-02 13:56:30 +0430

    Add logout

--- blog_project/blog/routes.py
+++ blog_project/blog/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from . import app, db, bcrypt
 from .forms import RegistrationForm, LoginForm
 from .models import User
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route('/')
@@ -27,16 +27,26 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        flash('you already logged in', 'info')
+        return redirect(url_for('home'))
+
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
             flash('you logged in successfully', 'info')
-            return redirect(url_for('home'))
+            return redirect(next_page if next_page else url_for('home'))
         else:
             flash('email or password is wrong', 'danger')
-    else:
-        flash('please enter correct email', 'danger')
 
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('you logged out successfully', 'info')
+    return redirect(url_for('home'))
