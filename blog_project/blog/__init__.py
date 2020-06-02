350c240b8ad7db160befc3df2b02edf65cfa5dd6
farhadmpr <farhadmp@gmail.com>
2020-06-02 10:52:48 +0430

    Add user login

--- blog_project/blog/__init__.py
+++ blog_project/blog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ app.config['SECRET_KEY'] = 'gfh65erwf54werdsfg546debdfs5f4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from . import routes
--- blog_project/blog/routes.py
+++ blog_project/blog/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, url_for, flash
 from . import app, db, bcrypt
 from .forms import RegistrationForm, LoginForm
 from .models import User
+from flask_login import login_user
 
 
 @app.route('/')
@@ -24,7 +25,18 @@ def register():
     
     return render_template('register.html', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash('you logged in successfully', 'info')
+            return redirect(url_for('home'))
+        else:
+            flash('email or password is wrong', 'danger')
+    else:
+        flash('please enter correct email', 'danger')
+
     return render_template('login.html', form=form)
