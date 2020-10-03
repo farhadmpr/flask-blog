87b832016bdd73c010da1df4991d6113a3d1bc41
Alireza <alirezarezaei8080@gmail.com>
2020-10-03 10:45:29 +0330

    Flask secret key is generated by system , randomly

--- blog_project/blog/__init__.py
+++ blog_project/blog/__init__.py
@@ -1,11 +1,12 @@
+import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'gfh65erwf54werdsfg546debdfs5f4'
+app.config['SECRET_KEY'] = os.urandom(30)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
