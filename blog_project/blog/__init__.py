cde7a3613e143904a3167065a0df1dce6d17a8a4
sayefiqb <sayefiqbal007@gmail.com>
2023-05-03 22:17:08 -0400

    category feature

--- blog_project/blog/__init__.py
+++ blog_project/blog/__init__.py
@@ -8,7 +8,10 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(30)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+# db.create_all()
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
