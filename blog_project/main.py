f8fd1dee942f6ecbfb555cfa39bf57024c6bdc7f
farhadmpr <farhadmp@gmail.com>
2023-05-07 10:17:12 +0330

    Create db

--- blog_project/main.py
+++ blog_project/main.py
@@ -1,5 +1,8 @@
-from blog import app
+from blog import app, db
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        
     app.run(host='0.0.0.0', debug=True, port=8080)
